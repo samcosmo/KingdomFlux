7163455bccfba432afb8dcc010f22af82e2803a5
bob <New@NEW-PC>
2017-02-08 21:50:47 +1100

    werwer

---
+++ KingdomFluxTextBased/KingdomFluxTextBased/list.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "list.h"
+
+
+list::list()
+{
+}
+
+
+list::~list()
+{
+}
+
+
+private: struct Node
+{
+
+};
---
+++ KingdomFluxTextBased/KingdomFluxTextBased/list.h
@@ -0,0 +1,8 @@
+#pragma once
+class list
+{
+public:
+	list();
+	~list();
+};
+
